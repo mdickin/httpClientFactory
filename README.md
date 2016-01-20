4c9a83159eb4e148ea7cc000da91713d8905c0e3
Matt Dickinson <matt@mdickin.com>
2016-01-19 21:01:18 -0600

    Added handler support Added HTTPS support Added HTTP agent support Renamed del to delete Initial commit of README

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
-# httpclient
+# Http Client Factory
+
+This library is aimed at improving the development experience for REST-like calls in
+NodeJS. It builds off the Node `http` library. It's inspired by the 
+System.Net.Http.HttpClientFactory library in .NET. 
+
+##Basic Usage
+
+```javascript
+var HttpClientFactory = require("httpclientfactory")
+
+//Issues a GET request to http://www.tempuri.org/my/endpoint?some=value&search=text
+var promise = HttpClientFactory.getClient()
+    .get("http://www.tempuri.org/my/endpoint", { some: "value", search: "text"})
+  
+promise.then(function (response) {
+    //Do something with the response
+})
+.catch(function (error) {
+    //Do something with the error
+})
+```
+
+## API
+`HttpClientFactory`
+- `.getClient()`
+ - Returns `HttpClient`
+
+HttpClient
+- `.addHandler(HttpClientHandler)`
+ - Returns `HttpClient`
+- `.get(url, RequestBody)`
+ - Returns `Promise`
+- `.post(url, RequestBody)`
+ - Returns `Promise`
+- `.put(url, RequestBody)`
+ - Returns `Promise`
+- `.delete(url, RequestBody)`
+ - Returns `Promise`
+- `.send(HttpRequest, RequestBody)`
+ - Returns `Promise`
+ - Sends a raw request
+
+HttpClientHandler
+- `onRequest: function (HttpRequest, body)`
+ - Reads or modifies request and/or body as needed
+ - Leave undefined if not needed
+- `onResponse: function (HttpResponse)`
+ - Reads or modifies response if needed
+ - Leave undefined if not needed
+ 
+RequestBody
+- Normal JSON object
+- For `.get()` and `.delete()` requests, this will be converted into query string parameters
+- For `.put()` and `.post()` requests, this will be sent as JSON content
+
+
+##Reading the Response
+The response is a typical HTTP response as defined by the `http` library,
+with the addition of a `body` property which contains the response content
+(in string form).
+
+
+##Handlers
+
+### Setting authorization headers
+```javascript
+var handler = {
+    onRequest: function (req) {
+        req.headers.authorization = "sampleAuthScheme authValue";
+    }   
+}
+
+//Issues a POST request to the URL 
+//with the authorization header set to "sampleAuthScheme authValue"
+//and a JSON payload  of { "postdata": "goes here" }
+HttpClientFactory.getClient()
+    .addHandler(handler)
+    .post("http://www.tempuri.org/my/endpoint", { postdata: "goes here" })
+```
+
+### Logging request/response data
+```javascript
+var traceLogHandler = {
+    onRequest: function (req, body) {
+        var logger = require("myTraceLogger")
+        logger.logRequest(req.href, req.headers, body)
+    },
+    onResponse: function (response) {
+        var logger = require("myTraceLogger")
+        logger.logResponse(response.headers, response.body)
+    } 
+}
+
+//Logs both request and response data to myTraceLogger
+HttpClientFactory.getClient()
+    .addHandler(traceLogHandler)
+    .post("http://www.tempuri.org/my/endpoint", { postdata: "goes here" })
+```
+
+##Promises
+
+`httpClientFactory` uses the [bluebird](http://www.npmjs.org/package/bluebird) library for promises
--- spec/httpClientFactorySpec.js
+++ spec/httpClientFactorySpec.js
@@ -3,17 +3,22 @@ var _proxyquire = require("proxyquire");
 var _httpMock = {
     request: function () {}
 };
+var _httpsMock = {
+    request: function () {}
+};
 _proxyquire("../lib/httpClientFactory", {
     "http": _httpMock,
+    "https": _httpsMock,
     "@noCallThru": true
 }); 
 var _httpClientFactory;
 
-describe("Basic HTTP calls", function () {
     
-    beforeEach(function () {
-        _httpClientFactory = require("../lib/httpClientFactory");
-    });
+beforeEach(function () {
+    _httpClientFactory = require("../lib/httpClientFactory");
+});
+
+describe("Basic HTTP calls", function () {
     
     it("sends delete method for delete", function() {
         //Arrange
@@ -22,7 +27,7 @@ describe("Basic HTTP calls", function () {
         });
         
         //Act
-        var result = _httpClientFactory.getClient().del("http://www.tempuri.org/some/path", { query: "param1", q2: "p2" }).value();
+        var result = _httpClientFactory.getClient().delete("http://www.tempuri.org/some/path", { query: "param1", q2: "p2" }).value();
         
         //Assert
         expect(result.statusCode).toBe(404);
@@ -139,51 +144,182 @@ describe("Basic HTTP calls", function () {
        expectRequestCalled(request, "this is not json");
     });
     
-    
-    
-    var _requestMock;
-    function setHttpResult(response, body) {
-        spyOn(_httpMock, "request").and.callFake(function (req, cb) {
-            _requestMock = _.extend({ write: function() {}}, req);
-            spyOn(_requestMock, "write");
-            response.on = function(eventName, func) {
-                if (eventName == "data") {
-                    func(JSON.stringify(body));
-                    return;
-                }
-                if (eventName == "end") {
-                    func();
-                }
-            };
-            cb(response);
-            _requestMock.end = function() {};
-            return _requestMock;
-        });
-    }
-    
-    function expectRequestCalled(request, body) {
-        expect(_httpMock.request)
-            .toHaveBeenCalledWith(jasmine.objectContaining(request), 
-                jasmine.any(Function));
-                
-        if (body) {
-            var bodyAsString;
-            if (body.constructor === String) {
-                bodyAsString = body;
-            } else {
-                bodyAsString = JSON.stringify(body);
-            }
-            expect(_requestMock.write).toHaveBeenCalledWith(bodyAsString);
+    it("allows sending requests over HTTPS", function () {
+        var response = {
+            some: "content"
+        };
+        setHttpResult({
+            statusCode: 200,
+        }, response, true);
+        var request = {
+            method: "post"
         }
-    }
-    
-    function expectRequestCalledRaw(request, body) {
-        expect(_httpMock.request)
-            .toHaveBeenCalledWith(jasmine.objectContaining(request), 
-                jasmine.any(Function));
-                
-        if (body) {
-            expect(_requestMock.write).toHaveBeenCalledWith(JSON.stringify(body));
-        }
-    }
+        
+       //Act
+       var result = _httpClientFactory.getClient().post(
+           "https://www.tempuri.org/some/path", request).value();
+       
+       //Assert 
+       expect(result.statusCode).toBe(200);
+       expectRequestCalled({
+            protocol: "https:",
+            host: "www.tempuri.org",
+            pathname: "/some/path",
+            method: "post"
+       }, request, true)
+    })
 });
+
+describe("HTTP agent", function () {
+    it("allows passing agent information", function () {
+        //Arrange
+        setHttpResult({ statusCode: 200 }, null, true);
+        var agentConfig = {
+            keepAlive: true
+        }
+        
+        //Act
+        _httpClientFactory.getClient(agentConfig).post(
+           "https://www.tempuri.org/some/path").value();
+        
+        //Assert
+        expectRequestCalled({
+            agent: jasmine.objectContaining({
+                keepAlive: true
+            })
+        }, null, true)
+    })  
+})
+
+describe("handlers", function () {
+    
+    it("allows handlers to modify the request header", function () {
+        //Arrange
+        setHttpResult({ statusCode: 200})
+        var handler = {
+            onRequest: function (req, body) {
+                req.headers.authorization = "someAuth authValue"
+            }
+        };
+        
+        //Act
+        _httpClientFactory.getClient()
+            .addHandler(handler)
+            .get("testuri");
+        
+        //Assert
+        expectRequestCalled({
+            headers: {
+                authorization: "someAuth authValue",
+                "Content-Type": "application/json"
+            }
+        })
+        
+    });
+    
+    it("allows handlers to modify the request body", function () {
+        //Arrange
+        setHttpResult({ statusCode: 200})
+        var handler = {
+            onRequest: function (req, body) {
+                body.test = "extra value"
+            }
+        };
+        
+        //Act
+        _httpClientFactory.getClient()
+            .addHandler(handler)
+            .post("testuri", {
+                data: 5
+            });
+        
+        //Assert
+        expectRequestCalled({}, {
+            data: 5,
+            test: "extra value"
+        })
+        
+    });
+    
+    it("allows handlers to modify the response header", function () {
+        //Arrange
+        setHttpResult({ statusCode: 200, headers: {}})
+        var handler = {
+            onResponse: function (req, body) {
+                req.headers.testVal = "updated header"
+            }
+        };
+        
+        //Act
+        var result = _httpClientFactory.getClient()
+            .addHandler(handler)
+            .get("testuri").value();
+        
+        //Assert
+        expect(result.headers.testVal).toBe("updated header")
+        
+    });
+    
+    it("allows handlers to modify the response body", function () {
+        //Arrange
+        setHttpResult({ statusCode: 200}, "return value")
+        var handler = {
+            onResponse: function (req) {
+                req.body += "-extra value"
+            }
+        };
+        
+        //Act
+        var result = _httpClientFactory.getClient()
+            .addHandler(handler)
+            .post("testuri", {
+                data: 5
+            }).value();
+        
+        //Assert
+        expect(result.body).toBe("return value-extra value")
+        
+    })
+})
+    
+var _requestMock;
+function setHttpResult(response, body, isHttps) {
+    var mock = isHttps ? _httpsMock : _httpMock;
+    var responseBody;
+    if (body) {
+        responseBody = body.constructor === String ? body : JSON.stringify(body)
+    }
+    spyOn(mock, "request").and.callFake(function (req, cb) {
+        _requestMock = _.extend({ write: function() {}}, req);
+        spyOn(_requestMock, "write");
+        response.on = function(eventName, func) {
+            if (eventName == "data") {
+                func(responseBody);
+                return;
+            }
+            if (eventName == "end") {
+                func();
+            }
+        };
+        cb(response);
+        _requestMock.end = function() {};
+        return _requestMock;
+    });
+}
+
+function expectRequestCalled(request, body, isHttps) {
+    var mock = isHttps ? _httpsMock : _httpMock;
+    expect(mock.request)
+        .toHaveBeenCalledWith(jasmine.objectContaining(request), 
+            jasmine.any(Function));
+            
+    if (body) {
+        var bodyAsString;
+        if (body.constructor === String) {
+            bodyAsString = body;
+        } else {
+            bodyAsString = JSON.stringify(body);
+        }
+        expect(_requestMock.write).toHaveBeenCalledWith(bodyAsString);
+    }
+}
